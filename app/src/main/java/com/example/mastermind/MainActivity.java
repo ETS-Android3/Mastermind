21d37c2dccc1f972d7a86677ff7bca6f2671e80e
Kania Ganda <kaniagandacs@gmail.com>
2021-12-02 23:01:43 -0800

    Changed number buttons to dynamic buttons

--- app/src/main/java/com/example/mastermind/MainActivity.java
+++ app/src/main/java/com/example/mastermind/MainActivity.java
@@ -7,6 +7,7 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -18,11 +19,10 @@ import java.util.Arrays;
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = "MainActivity";
-    
+
+    private LinearLayout llContainerNumbers;
     private TextView tvGuessBox1;
     private TextView tvGuessBox2;
-    private Button btnNumber1;
-    private Button btnNumber2;
     private Button btnResetGuess;
     private Button btnSubmitGuess;
 
@@ -30,6 +30,8 @@ public class MainActivity extends AppCompatActivity {
     private ArrayList<TextView> listGuessBoxes;
     private ArrayList<Button> listPossibleNumbers;
     private int secretNumberLength;
+    private int numberMin;
+    private int numberMax;
     private int currentGuessPosition = 0;
 
     @Override
@@ -37,33 +39,22 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        llContainerNumbers = findViewById(R.id.llContainerNumbers);
+
         tvGuessBox1 = findViewById(R.id.tvGuessBox1);
         tvGuessBox2 = findViewById(R.id.tvGuessBox2);
-        btnNumber1 = findViewById(R.id.btnNumber1);
-        btnNumber2 = findViewById(R.id.btnNumber2);
         btnResetGuess = findViewById(R.id.btnResetGuess);
         btnSubmitGuess = findViewById(R.id.btnSubmitGuess);
 
         secretNumberLength = 2;
         secretNumber = new ArrayList<>(Arrays.asList("2", "2"));
-        listGuessBoxes = new ArrayList<>(Arrays.asList(tvGuessBox1, tvGuessBox2));
-        listPossibleNumbers = new ArrayList<>(Arrays.asList(btnNumber1, btnNumber2));
 
-        for (Button buttonNumber : listPossibleNumbers) {
-            buttonNumber.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    if (currentGuessPosition < secretNumberLength) {
-                        TextView currentGuessBox = listGuessBoxes.get(currentGuessPosition);
-                        currentGuessBox.setText(buttonNumber.getText());
-                        ++currentGuessPosition;
-                    }
-                    else {
-                        Toast.makeText(MainActivity.this, "Submit or reset guess", Toast.LENGTH_SHORT).show();
-                    }
-                }
-            });
-        }
+        numberMin = 1;
+        numberMax = 2;
+        listGuessBoxes = new ArrayList<>(Arrays.asList(tvGuessBox1, tvGuessBox2));
+
+        createNumberButtons();
+
         btnResetGuess.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -106,6 +97,35 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    // Create button for each possible number
+    private void createNumberButtons() {
+        for (int i = numberMin; i <= numberMax; ++i) {
+            final Button button = new Button(this);
+            button.setLayoutParams(new LinearLayout.LayoutParams(150, 150));
+            button.setId(i);
+            button.setText(Integer.toString(i));
+            Log.i(TAG, "Button id: " + button.getId() + ", Button text: " + button.getText());
+
+            // Update current guess box with number clicked
+            button.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (currentGuessPosition < secretNumberLength) {
+                        TextView currentGuessBox = listGuessBoxes.get(currentGuessPosition);
+                        currentGuessBox.setText(button.getText());
+                        ++currentGuessPosition;
+                    }
+                    else {
+                        Toast.makeText(MainActivity.this, "Submit or reset guess", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            });
+
+            llContainerNumbers.addView(button);
+        }
+    }
+
+    // Reset all guess boxes
     private void resetGuessBoxes() {
         for (TextView guessBox : listGuessBoxes) {
             guessBox.setText("?");
